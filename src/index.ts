8cb4b35a764c17021df38773e02f68c95e0c74c5
Benjamín Vicente <projects@benjavicente.dev>
2025-11-16 22:09:21 -0300

    smaller id generator

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
+import { DurableObject } from 'cloudflare:workers';
+
 const WELCOME_TEXT =
 	'{"name":"PeerJS Server","description":"A server side element to broker connections between PeerJS clients.","website":"https://peerjs.com/"}';
 const HEARTBEAT = '{"type":"HEARTBEAT"}';
 const OPEN = '{"type":"OPEN"}';
 const ID_TAKEN = '{"type":"ID-TAKEN","payload":{"msg":"ID is taken"}}';
 
-export class PeerServerDO implements DurableObject {
-	constructor(
-		private state: DurableObjectState,
-		private env: Env,
-	) {
-		this.state.setWebSocketAutoResponse(new WebSocketRequestResponsePair(HEARTBEAT, HEARTBEAT));
+export class PeerServerDO extends DurableObject {
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+		this.ctx.setWebSocketAutoResponse(new WebSocketRequestResponsePair(HEARTBEAT, HEARTBEAT));
 	}
+
+	public async getNextId() {
+		const lenght = 6;
+		const id = (await this.ctx.storage.get<number>('id')) ?? 0;
+
+		const decoded = new TextEncoder().encode(id.toString());
+		const hash = await crypto.subtle.digest('SHA-256', decoded).then((buffer) =>
+			Array.from(new Uint8Array(buffer))
+				.map((b) => b.toString(16).padStart(2, '0'))
+				.join('')
+				.slice(0, lenght)
+		);
+
+		await this.ctx.storage.put('id', id + 1);
+		return hash;
+	}
+
 	async fetch(request: Request) {
 		const url = new URL(request.url);
 		const id = url.searchParams.get('id');
@@ -18,7 +35,7 @@ export class PeerServerDO implements DurableObject {
 		if (!id || !token) return new Response(null, { status: 400 });
 		const [wsclient, wsserver] = Object.values(new WebSocketPair());
 
-		const existingWss = this.state.getWebSockets(id);
+		const existingWss = this.ctx.getWebSockets(id);
 		if (existingWss.length > 0 && existingWss[0].deserializeAttachment().token !== token) {
 			wsserver.accept();
 			wsserver.send(ID_TAKEN);
@@ -28,7 +45,7 @@ export class PeerServerDO implements DurableObject {
 			existingWss.forEach((ws) => ws.close(1000));
 		}
 
-		this.state.acceptWebSocket(wsserver, [id]);
+		this.ctx.acceptWebSocket(wsserver, [id]);
 		wsserver.serializeAttachment({ id, token });
 		wsserver.send(OPEN);
 
@@ -36,7 +53,7 @@ export class PeerServerDO implements DurableObject {
 	}
 	webSocketMessage(ws: WebSocket, message: string): void | Promise<void> {
 		const msg = JSON.parse(message);
-		const dstWs = this.state.getWebSockets(msg.dst)[0];
+		const dstWs = this.ctx.getWebSockets(msg.dst)[0];
 		msg.src = ws.deserializeAttachment().id;
 		dstWs.send(JSON.stringify(msg));
 	}
@@ -49,18 +66,21 @@ export default {
 		switch (url.pathname) {
 			case '/':
 				return new Response(WELCOME_TEXT);
-			case '/peerjs':
-				let objId = env.PEER_SERVER.idFromName(url.host);
-				let stub = env.PEER_SERVER.get(objId);
+			case '/peerjs': {
+				const stub = env.PEER_SERVER.getByName(url.host);
 				return stub.fetch(request);
-			case '/peerjs/id':
-				return new Response(crypto.randomUUID(), {
+			}
+			case '/peerjs/id': {
+				const stub = env.PEER_SERVER.getByName(url.host);
+				const id = await stub.getNextId();
+				return new Response(id, {
 					status: 200,
 					headers: {
 						'Content-Type': 'text/plain',
 						'Access-Control-Allow-Origin': '*',
 					},
 				});
+			}
 			default:
 				return new Response(null, { status: 404 });
 		}
